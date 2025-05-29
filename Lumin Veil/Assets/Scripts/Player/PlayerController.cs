d744f0058ee56f15bb9baba614d6e0754e21e337
Fate2664 <18pielou@gmail.com>
2025-05-29 12:11:24 +0200

    Added crouch functionality to the controller

--- Lumin Veil/Assets/Scripts/Player/PlayerController.cs
+++ Lumin Veil/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,39 +9,72 @@ public class PlayerController : MonoBehaviour
     [Space(10)]
     [Range(0, 10)]
     [SerializeField] private float moveSpeed;
+    [Range(0, 1)]
+    [SerializeField] private float crouchSpeedMultiplier;
     [Range(5, 15)]
     [SerializeField] private float jumpForce;
+
+    [Header("Player Setup")]
+    [Space(10)]
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform ceilingCheck;
+    [SerializeField] private Collider2D crouchDisableCollider;
 
     [Header("Animator Component")]
     [Space(10)]
     [SerializeField] private Animator animator;
 
 
+    [Header("Events")]
+    [Space]
+    public UnityEvent OnLandEvent;
 
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    public BoolEvent OnCrouchEvent;
+    private bool wasCrouching = false;
+
+    private const float groundedRadius = .2f;
+    private const float ceilingRadius = .2f;
+    private bool grounded;
     private float moveDir;
-    private bool canJump = false;
+    private bool shouldJump = false;
+    private bool shouldCrouch = false;
     private Rigidbody2D rb;
 
-
-
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        if (OnLandEvent == null)
+        {
+            OnLandEvent = new UnityEvent();
+        }
+        if (OnCrouchEvent == null)
+        {
+            OnCrouchEvent = new BoolEvent();
+        }
     }
+
+
 
     void Update()
     {
+        shouldJump = false;
+        shouldCrouch = false;
+
         moveDir = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce * 100));
-            if (animator != null)
-            {
-                animator.SetBool("isJumping", true);
-            }
-            canJump = false;
+            shouldJump = true;
         }
-        MoveCharacter(moveDir);
+        if (Input.GetKey(KeyCode.LeftControl) && grounded)
+        {
+            shouldCrouch = true;
+        }
+
+        MoveCharacter(moveDir,moveSpeed, shouldCrouch ,shouldJump);
     }
 
     private void FixedUpdate()
@@ -53,23 +87,77 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
-    }
+
+        bool wasGrounded = grounded;
+        grounded = false;
 
 
-    private void MoveCharacter(float direction)
-    {
-        rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
         {
-            if (animator != null)
+            if (colliders[i].gameObject != gameObject)
             {
-                animator.SetBool("isJumping", false);
+                grounded = true;
+                if (!wasGrounded)
+                {
+                    OnLandEvent.Invoke();
+                }
             }
-            canJump = true;
         }
     }
+
+
+    private void MoveCharacter(float direction,float moveSpeed, bool crouch, bool jump)
+    {
+        if (!crouch)
+        {
+            if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+            {
+                crouch = true;
+            }
+        }
+
+        if (grounded)
+        {
+            if (crouch)
+            {
+                if (!wasCrouching)
+                {
+                    wasCrouching = true;
+                    OnCrouchEvent.Invoke(true);
+                }
+
+
+                if (crouchDisableCollider != null)
+                {
+                    crouchDisableCollider.enabled = false;
+                }
+            }
+            else
+            {
+                if (crouchDisableCollider != null)
+                {
+                    crouchDisableCollider.enabled = true;
+                }
+                if (wasCrouching)
+                {
+                    wasCrouching = false;
+                    OnCrouchEvent.Invoke(false);
+                }
+            }
+        }
+        float modifiedSpeed = crouch ? moveSpeed * crouchSpeedMultiplier : moveSpeed;
+        rb.linearVelocity = new Vector2(direction * modifiedSpeed, rb.linearVelocity.y);
+
+        if (grounded && jump)
+        {
+            grounded = false;
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce * 100));
+            if (animator != null)
+            {
+                animator.SetBool("isJumping", true);
+            }
+        }
+    }
+
 }
